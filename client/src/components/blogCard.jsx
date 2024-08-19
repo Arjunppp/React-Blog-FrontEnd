79662cd9968a38fe01ee3fc143304cdc25116e4f
Arjunppp <arjunmohanak@gmail.com>
2024-08-19 16:07:04 +0530

    conditonal rendering based on the length added

--- client/src/components/blogCard.jsx
+++ client/src/components/blogCard.jsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
 
 
 
 export const BlogCard = ({title , content}) => 
 {
+    const [isShowMore , setShowMore] =useState(false);
+
+
+    const shorter = content.slice(0,100);
+    
+    
+    const toggleState = () => 
+    {
+        if(isShowMore == true)
+        {
+            setShowMore(false);
+        }
+        else{
+
+            setShowMore(true);
+        }
+
+
+    }
+
     return(
         <div className="blogCard">
             <h1>{title}</h1>
-            <p>
-                {content}
+            <p className="Blog_Content">
+               {isShowMore ? shorter : content}
             </p>
+      {(content.length > 100) &&   <button onClick={toggleState}>{isShowMore? 'showless' : 'showMore'}</button>}
+        
         </div>
     )
 
